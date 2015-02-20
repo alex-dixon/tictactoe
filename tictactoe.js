03c5ee94a1a1054445c1c554ee86f05e7df793ae
Alex Dixon <email@alexdixon.io>
2015-02-20 06:28:10 -0800

    Fixed minus 1 error in marks function

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,9 @@
+//TODO. 1. Comp doesn't move first
+//TODO. 2. Player can click on a comp move and move
+
+//TODO. 4. game.init() should reset the game, or game.reset() should do this
+
+
 var game = {
 
 	turn: 0,
@@ -42,6 +48,7 @@ var game = {
 			$(this).append('<h2>' + game.player + '</h2>');
 			game.prevMove = '#' + $(this).attr('id');
 			var playerMarks = game.marks(game.player);
+			//game.openSpaces = game.disableSpace(playerMarks, game.openSpaces);
 			if (game.openSpaces.length > 0) {
 				game.compTurn();
 			} else {
@@ -57,32 +64,19 @@ var game = {
 	compTurn: function compTurn () {
 		// get all player moves
 		var playerMoves = game.marks(game.player);
-
 		// check player win
 		var playerWin = game.checkWin(playerMoves);
-
-		if (playerWin) {
-			console.log('player win message here');
-		} else if (compWin) {
-			console.log('comp win message here');
-		}
-
 		// set avail spaces after player moves
 		game.openSpaces = game.disableSpace(game.prevMove, game.openSpaces);
 		var idx = Math.floor((Math.random() * game.openSpaces.length));
-
 		// draw letter to view
 		$(game.openSpaces[idx]).append('<h2>' + game.comp + '</h2>');
-
 		// set prevMove to last move
 		game.prevMove = '#' + $(game.openSpaces[idx]).attr('id');
-
 		// remove lastMove from openSpaces
 		game.openSpaces = game.disableSpace(game.prevMove, game.openSpaces);
-
 		// get all comp moves to check if comp win
 		var compMarks = game.marks(game.comp);
-
 		if (compMarks.length > 0) {
 			// check for comp win
 			var compWin = game.checkWin(compMarks);
@@ -102,7 +96,11 @@ var game = {
 		//}
 		//}
 	},
-
+	//TODO. Debug disableSpace(). Check parameters and loops.
+	//resets openSpaces array based on where marks(computer or player) exist
+	//marks should be one value to remove so that only one space is removed per turn,
+	// instead of all moves made by player or computer like it is now
+	//prevMove needs to be content/idname, not idx
 	disableSpace: function disableSpace (lastMove, remaining) {
 		remaining = remaining || this.openSpaces;
 		for (var i = 0; i < remaining.length; i++) {
@@ -117,7 +115,7 @@ var game = {
 	marks: function marks (icon) {
 		var played = [];
 		var loc;
-		for (var i = 0; i < game.board.length - 1; i++) {
+		for (var i = 0; i < game.board.length; i++) {
 			//var loc = $(game.board[i]).find('h2').text();
 			if ( icon === $(game.board[i]).find('h2').text() ) {
 				played.push(i);
