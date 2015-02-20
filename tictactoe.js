3f7fee3265e1c12e2e13b4671593d7cc51dbf691
Alex Dixon <email@alexdixon.io>
2015-02-20 05:45:13 -0800

    Win check works partially

--- tictactoe.js
+++ tictactoe.js
@@ -5,12 +5,6 @@ var game = {
 	comp: '',
 	prevMove: '',
 
-	init: function() {
-		this.turn = 0;
-		this.player = prompt('X XOR O?');
-		this.comp = this.player === 'X' ? 'O' : 'X';
-	},
-
 	board: [
 		'#top-left', 			//0
 		'#top-middle', 		//1
@@ -35,14 +29,19 @@ var game = {
 		'#bottom-right' 	//8
 	],
 
+	init: function init () {
+		this.turn = 0;
+		this.player = prompt('X XOR O?');
+		this.comp = this.player === 'X' ? 'O' : 'X';
+	},
 
-	playerTurn: function () {
+
+	playerTurn: function playerTurn () {
 		var allSquares = this.openSpaces.join(', ');
 		$(allSquares).one('click', function () {
 			$(this).append('<h2>' + game.player + '</h2>');
 			game.prevMove = '#' + $(this).attr('id');
-			//var playerMarks = game.marks(game.player);
-			//game.openSpaces = game.disableSpace(playerMarks, game.openSpaces);
+			var playerMarks = game.marks(game.player);
 			if (game.openSpaces.length > 0) {
 				game.compTurn();
 			} else {
@@ -51,29 +50,46 @@ var game = {
 		});
 	},
 
-	over: function() {
+	over: function over () {
 		console.log('game over');
 	},
 
-	compTurn: function () {
-		//var playerMarks = game.marks(game.player);
+	compTurn: function compTurn () {
+		// get all player moves
+		var playerMoves = game.marks(game.player);
 
+		// check player win
+		var playerWin = game.checkWin(playerMoves);
+
+		if (playerWin) {
+			console.log('player win message here');
+		} else if (compWin) {
+			console.log('comp win message here');
+		}
+
+		// set avail spaces after player moves
 		game.openSpaces = game.disableSpace(game.prevMove, game.openSpaces);
 		var idx = Math.floor((Math.random() * game.openSpaces.length));
+
 		// draw letter to view
 		$(game.openSpaces[idx]).append('<h2>' + game.comp + '</h2>');
+
 		// set prevMove to last move
 		game.prevMove = '#' + $(game.openSpaces[idx]).attr('id');
+
 		// remove lastMove from openSpaces
 		game.openSpaces = game.disableSpace(game.prevMove, game.openSpaces);
-		//
-		//var compMarks = game.marks(game.comp);
-		//if (compMarks.length > 0) {
-		//	// check for comp win
-		//	var compWin = game.checkWin(game.comp, compMarks);
-		//}
-		//if (compWin) {
-		//	console.log('compWin');
+
+		// get all comp moves to check if comp win
+		var compMarks = game.marks(game.comp);
+
+		if (compMarks.length > 0) {
+			// check for comp win
+			var compWin = game.checkWin(compMarks);
+		}
+		if (compWin) {
+			console.log('compWin');
+		}
 		//	//this.gameOver();
 		//} else {
 		//	console.log('line65');
@@ -86,11 +102,7 @@ var game = {
 		//}
 		//}
 	},
-	//TODO. Debug disableSpace(). Check parameters and loops.
-	//resets openSpaces array based on where marks(computer or player) exist
-	//marks should be one value to remove so that only one space is removed per turn,
-	// instead of all moves made by player or computer like it is now
-	//prevMove needs to be content/idname, not idx
+
 	disableSpace: function disableSpace (lastMove, remaining) {
 		remaining = remaining || this.openSpaces;
 		for (var i = 0; i < remaining.length; i++) {
@@ -101,7 +113,7 @@ var game = {
 		return remaining;
 	},
 
-	// returns array of all game.board index that has specified icon
+	// returns array showing where all player or comp marks are
 	marks: function marks (icon) {
 		var played = [];
 		var loc;
@@ -136,47 +148,47 @@ var game = {
 
 
 	// >> currentPositions();
-	checkWin: function (currentBoard) {
-		var check = function (num) {
-			currentBoard.some(function (elem) { return elem === num })
-		};
+	checkWin: function checkWin (moves) {
+		//var check = function (testNum) {
+		//	moves.some(function (elem) { return elem === testNum });
+		//};
 	// sort
-	//currentBoard = currentBoard.sort(function (a, b) { return a < b });
-		if (currentBoard.length > 2) {
+	//moves = moves.sort(function (a, b) { return a < b });
+		if (moves.length > 2) {
 
 		// win combo?
-			if (check(0)) {
-				if (check(1)) {
-					if (check(2)) {
+			if (moves.check(0)) {
+				if (moves.check(1)) {
+					if (moves.check(2)) {
 						console.log('win');
 					}
 				}
-				if (check(3)) {
-					if (check(6)) {
+				if (moves.check(3)) {
+					if (moves.check(6)) {
 						console.log('win');
 					}
 				}
-				if (check(4)) {
-					if (check(8)) {
+				if (moves.check(4)) {
+					if (moves.check(8)) {
 						console.log('win');
 					}
 				}
 			}
-			if (check(1) && check(4) && check(7)) {
+			if (moves.check(1) && moves.check(4) && moves.check(7)) {
 				console.log('win');
 			}
-			if (check(2)) {
-				if (check(4) && check(6)) {
+			if (moves.check(2)) {
+				if (moves.check(4) && moves.check(6)) {
 					console.log('win');
 				}
-				if (check(5) && check(8)) {
+				if (moves.check(5) && moves.check(8)) {
 					console.log('win');
 				}
 			}
-			if (check(3) && check(4) && check(5)) {
+			if (moves.check(3) && moves.check(4) && moves.check(5)) {
 				console.log('win');
 			}
-			if (check(6) && check(7) && check(8)) {
+			if (moves.check(6) && moves.check(7) && moves.check(8)) {
 				console.log('win');
 			}
 
@@ -187,10 +199,15 @@ var game = {
 game.init();
 	// while (!win) {
 //game.compTurn();
-//game.playerTurn();
+game.playerTurn();
 	// }
 	//game.over();
 
+Array.prototype.check = function(e) {
+	return this.some(function(a) {
+		return a === e;
+	});
+};
 
 // jQuery that I can't get to play nice
 // $(document).ready(function () {
